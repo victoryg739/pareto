738611b68194675199f5072815da3c88ad9a36d0
AloysiusTan <97999822+AloysiusTan@users.noreply.github.com>
2023-01-14 20:59:31 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import React from "react"
 import Signup from "./pages/Signup"
-import { Container } from "react-bootstrap"
+//import { Container } from "react-bootstrap"
 import { AuthProvider } from './contexts/AuthContext';
 import ForgotPassword from "./pages/ForgotPassword"
 import UpdateProfile from "./pages/UpdateProfile"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-       </Routes>
+      <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/update-password" element={<UpdateProfile/>}/>
+      </Routes>
+      </AuthProvider>
   </BrowserRouter>
   
   );
