ff96fa3007913bdd17508337970d0242552971aa
AloysiusTan <97999822+AloysiusTan@users.noreply.github.com>
2023-01-14 20:04:25 +0800

    Routing Updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,7 @@
 
 import navbarCss from './navbar.module.css'
+import { Link } from 'react-router-dom'
+
 function navbar(){
   //test
   // commit
@@ -7,8 +9,12 @@ function navbar(){
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand">Navbar</a>
         <form className="form-inline">
-          <button type="button" className={`${navbarCss.navBtn} btn btn-outline-success`}>Login</button>
-          <button type="button" className={`${navbarCss.navBtn} btn btn-outline-danger`}>Sign up</button>
+          <button type="button" className={`${navbarCss.navBtn} btn btn-outline-success`}>
+            <Link to="/login">Login</Link>
+          </button>
+          <button type="button" className={`${navbarCss.navBtn} btn btn-outline-danger`}>
+          <Link to="/signup">Sign Up</Link>
+          </button>
         </form>
       </nav>
 
