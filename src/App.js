991209cc4e682bbace96c89902c42d21b89790ed
victor <victoryoong570@gmail.com>
2023-01-14 20:43:26 +0800

    tet

--- src/App.js
+++ src/App.js
@@ -2,44 +2,10 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-<<<<<<< HEAD
-import React from "react"
-import Signup from "./pages/Signup"
-import { Container } from "react-bootstrap"
-import { AuthProvider } from './contexts/AuthContext';
-//import { BrowserRouter as Router, Route } from "react-router-dom"
-// import Dashboard from "./pages/Dashboard"
-//import PrivateRoute from "./pages/PrivateRoute"
-import ForgotPassword from "./pages/ForgotPassword"
-import UpdateProfile from "./pages/UpdateProfile"
-=======
 import Dashboard from "./pages/Dashboard";
->>>>>>> d8f2c6b (test)
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <BrowserRouter>
-          <AuthProvider>
-            <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/update-profile" element={<UpdateProfile/>} />
-              <Route path="/signup" element={<Signup/>}/>
-              <Route path="/login" element={<Login/>} />
-              <Route path="/forgot-password" element={<ForgotPassword/>} />
-            </Routes>
-          </AuthProvider>
-        </BrowserRouter>
-      </div>
-    </Container>
-  )
-} 
-=======
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -50,6 +16,5 @@ function App() {
   
   );
 }
->>>>>>> f2025ed (test)
 
 export default App;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react"
-import { Card, Button, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
-
-export default function Dashboard() {
-  const [error, setError] = useState("")
-  const { currentUser, logout } = useAuth()
-  const history = useNavigate()
-
-  async function handleLogout() {
-    setError("")
-
-    try {
-      await logout()
-      history.push("/login")
-    } catch {
-      setError("Failed to log out")
-    }
-  }
-
-  return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
-    </>
-  )
-}
-=======
 import StockTable from "../components/StockTable";
 
 function Dashboard(){
@@ -49,4 +6,3 @@ function Dashboard(){
     );
 }
 export default Dashboard;
->>>>>>> d8f2c6b (test)
